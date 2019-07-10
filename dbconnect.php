ad2986e18ad12e4d9a9d43c89ae171d132d8bc32
Hector Ledesma <hectoremil21@hotmail.com>
2019-07-09 23:31:02 -0400

    Add remote mySQL database connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$serverName = "localhost";
-$username = "root";
-$password = "1234";
-$name = "list";
+$serverName = "remotemysql.com";
+$username = "tM4qU1hK9s";
+$password = "5jK2TGm0gB";
+$name = "tM4qU1hK9s";
 
 $conn = mysqli_connect($serverName, $username, $password, $name);
 
